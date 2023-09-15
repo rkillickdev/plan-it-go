2809e0130ae052dd3cc3889cba5e0f469ed0b573
Rob Killick <robkillick@me.com>
2023-09-15 21:53:06 +0000

    feat: create Trip model and register in admin file

--- trips/admin.py
+++ trips/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Trip
 
-# Register your models here.
+@admin.register(Trip)
+class TripAdmin(admin.ModelAdmin):
+
+    prepopulated_fields = {'slug': ('title',)}
--- trips/models.py
+++ trips/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from .models import Profile
+from . models import Location
 
-# Create your models here.
+
+class Trip(models.Model):
+    """
+    Model for creation of a Trip.
+    """
+
+    profile = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, related_name="trips"
+    )
+    location = models.ForeignKey(
+        Location, on_delete=models.CASCADE, related_name="trips"
+    )
+    places = models.ManyToManyField(
+        Place, related_name="trip_place", blank=True
+    )
+    title = models.CharField(max_length=30, unique=True)
+    slug = models.SlugField(max_length=30, unique=True)
+    description = models.TextField(blank=True)
+    start_date = models.DateField(blank=True)
+    end_date = models.DateField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    is_complete = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["-start_date"]
+
+    def __str__(self):
+        return self.title
+
+    def number_of_places(self):
+        return self.places.count()

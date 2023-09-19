981cdfd2c14645853a8e4b60145e223109536be3
Rob Killick <robkillick@me.com>
2023-09-19 11:08:42 +0000

    feat: add LoginRequiredMixin to trip views and filter queryset to only display trips linked to user profile

--- trips/views.py
+++ trips/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import (
     CreateView, ListView,
@@ -9,7 +10,7 @@ from .models import Profile
 from .forms import TripForm
 
 
-class TripCreateView(CreateView):
+class TripCreateView(LoginRequiredMixin, CreateView):
     """
     View to render the profile update form.
     """
@@ -17,14 +18,30 @@ class TripCreateView(CreateView):
     model = Trip
     success_url = reverse_lazy('trip_list')
 
-    # Populate profile field of the model Trip with the profile linked to the
-    # logged in user.
-    # https://stackoverflow.com/questions/18246326/how-do-i-set-user-field-in-form-to-the-currently-logged-in-user
+    """
+    Populate profile field of the model Trip with the profile linked to the
+    logged in user.
+    https://stackoverflow.com/questions/18246326/how-do-i-set-user-field-in-form-to-the-currently-logged-in-user
+    """
     def form_valid(self, form):
         form.instance.profile = Profile.objects.get(user=self.request.user)
         return super().form_valid(form)
 
 
-class TripListView(ListView):
+class TripListView(LoginRequiredMixin, ListView):
+    """
+    View to render a list of trips linked to the profile of the logged in user
+    """
     model = Trip
     context_object_name = "trip_list"
+
+    """
+    Filters queryset so only trips linked to the profile of the logged in
+    user are available to view.
+    Referenced this article when investigating how to achieve this:
+    https://stackoverflow.com/questions/59408167/list-of-current-user-objects-in-django-listview
+    """
+    def get_queryset(self):
+        return Trip.objects.filter(
+            profile=self.request.user.profile
+        ).order_by('-start_date')

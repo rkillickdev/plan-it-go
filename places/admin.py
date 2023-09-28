e21d716f807f44780e08f2e2db91296b8deae6a5
Rob Killick <robkillick@me.com>
2023-09-28 12:20:31 +0000

    feat: register image model in admin file and create a method to approve images

--- places/admin.py
+++ places/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Place
 from .models import Review
+from .models import Image
 
 admin.site.register(Place)
 
@@ -9,4 +10,11 @@ admin.site.register(Place)
 class ReviewAdmin(admin.ModelAdmin):
 
     def approve_reviews(self, request, queryset):
+        queryset.update(approved=True)
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+
+    def approve_images(self, request, queryset):
         queryset.update(approved=True)

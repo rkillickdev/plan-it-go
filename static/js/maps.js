7a6eabafc468c653c2c28d07364e8b2efbfb12ca
Rob Killick <robkillick@me.com>
2023-10-06 17:16:11 +0000

    feat: use json_script template tag to securely pass maps api key to javascript file

--- static/js/maps.js
+++ static/js/maps.js
@@ -1,11 +1,33 @@
+const mapkey = JSON.parse(document.getElementById('maps-api-key').textContent);
+
+// Load Google Maps API using Dynamic Library Import
+
+(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
+          key: mapkey,
+          v: "weekly",
+          // Use the 'v' parameter to indicate the version to use (weekly, beta, alpha, etc.).
+          // Add other bootstrap parameters as needed, using camel case.
+        });
+
+const latitude = JSON.parse(document.getElementById('latitude').textContent);
+const longitude = JSON.parse(document.getElementById('longitude').textContent);
+
+let num_lat = parseFloat(latitude)
+let num_long = parseFloat(longitude)
+
+console.log(latitude)
+console.log(longitude)
+console.log(num_lat)
+console.log(num_long)
+
 let map;
 
 async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
 
   map = new Map(document.getElementById("map"), {
-    center: { lat: 40.78343500, lng: -73.96625000 },
-    zoom: 10,
+    center: { lat: num_lat, lng: num_long },
+    zoom: 18,
   });
 }
 

6b4622a94f84e4fb66267eb6c50f06e85051e744
Rob Killick <robkillick@me.com>
2023-10-09 14:06:30 +0000

    feat: work on styling and layout of review form

--- places/forms.py
+++ places/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.urls import reverse
 from django.forms import ModelForm
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit, Layout, Hidden, Fieldset, Field
+from crispy_forms.layout import Submit, Layout, Hidden, Fieldset, Field, Div, HTML
 from .models import Place
 from .models import Review
 from .models import Image
@@ -29,18 +29,30 @@ class ReviewForm(ModelForm):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_id = 'reviewForm'
-        self.helper.form_class = 'blueForms'
+        self.helper.form_show_labels = False
         self.helper.form_method = "POST"
 
         self.helper.layout = Layout(
-            Fieldset(
-                'Leave A Review for {{place.name}}',
-                'body',
-                'user_rating',
-                'recommended'
-            ),
-            Submit('Submit', 'Post Review', css_id='submitButton')
-        )
+            Div(
+                HTML("""
+                <h2>Leave A Review for {{place.name}}</h2>
+                """), css_class="py-2"),
+            Field('body', placeholder='Tell us about your visit...'),
+            Div(
+                Submit('Submit', 'Post Review', css_id='submitButton'),
+                css_class="my-4"
+            )
+            
+
+            )
+
+        # self.helper.layout = Layout(
+        #     Fieldset(
+        #         'Leave A Review for {{place.name}}',
+        #         Field('body', placeholder='Tell us about your visit...')
+        #     ),
+        #     Submit('Submit', 'Post Review', css_id='submitButton')
+        # )
 
     class Meta:
         model = Review

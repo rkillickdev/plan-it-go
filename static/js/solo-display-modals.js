feb48fd149620a270ca2b1c247ba1ad13d988ea3
Rob Killick <robkillick@me.com>
2023-10-15 07:31:40 +0000

    refactor: modify structure of html for review of  display cards

--- static/js/solo-display-modals.js
+++ static/js/solo-display-modals.js
@@ -1,3 +1,5 @@
+//Solo Image Modal
+
 let soloImages = document.getElementsByClassName("image-solo");
 let modalSoloImage = document.getElementById("solo-modal-image");
 
@@ -9,6 +11,8 @@ for (let image of soloImages) {
 
     });
 }
+
+// Solo Review Modal
 
 let soloReviews = document.getElementsByClassName("review-solo");
 let SoloReviewProfile = document.getElementById("solo-review-profile");
@@ -24,5 +28,23 @@ for(let review of soloReviews) {
         SoloReviewProfile.src = reviewImagePath;
         SoloReviewTitle.innerText = reviewTitle;
         SoloReviewBody.innerText = reviewBody;
+    })
+}
+
+let userReviews = document.getElementsByClassName("user-review");
+// let userReviewProfile = document.getElementById("solo-review-profile");
+let userReviewTitle = document.getElementById("user-review-title");
+let userReviewBody = document.getElementById("user-review-body");
+
+for(let review of userReviews) {
+    review.addEventListener("click", (e) => {
+        let reviewId = e.currentTarget.getAttribute("review_id");
+        console.log(reviewId)
+        // let reviewImagePath = e.target.getAttribute("src");
+        // let reviewTitle = document.getElementById(`review-title${reviewId}`).innerText;
+        let reviewBody = document.getElementById(`review-body${reviewId}`).innerText;
+        // SoloReviewProfile.src = reviewImagePath;
+        // SoloReviewTitle.innerText = reviewTitle;
+        userReviewBody.innerText = reviewBody;
     })
 }

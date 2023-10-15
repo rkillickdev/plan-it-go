3aa28aa4a9a8ecf0a1cb2f973d2c288a94ee97fb
Rob Killick <robkillick@me.com>
2023-10-15 13:06:59 +0000

    feat: implement modal functionality for destination summary

--- static/js/solo-display-modals.js
+++ static/js/solo-display-modals.js
@@ -15,9 +15,9 @@ for (let image of soloImages) {
 // Solo Review Modal
 
 let soloReviews = document.getElementsByClassName("review-solo");
-let SoloReviewProfile = document.getElementById("solo-review-profile");
-let SoloReviewTitle = document.getElementById("solo-review-title");
-let SoloReviewBody = document.getElementById("solo-review-body");
+let soloReviewProfile = document.getElementById("solo-review-profile");
+let soloReviewTitle = document.getElementById("solo-review-title");
+let soloReviewBody = document.getElementById("solo-review-body");
 
 for(let review of soloReviews) {
     review.addEventListener("click", (e) => {
@@ -25,11 +25,39 @@ for(let review of soloReviews) {
         let reviewImagePath = e.target.getAttribute("src");
         let reviewTitle = document.getElementById(`review-title${reviewId}`).innerText;
         let reviewBody = document.getElementById(`review-body${reviewId}`).innerText;
-        SoloReviewProfile.src = reviewImagePath;
-        SoloReviewTitle.innerText = reviewTitle;
-        SoloReviewBody.innerText = reviewBody;
+        soloReviewProfile.src = reviewImagePath;
+        soloReviewTitle.innerText = reviewTitle;
+        soloReviewBody.innerText = reviewBody;
     })
 }
+
+
+// Solo Destination Info Modal
+
+let destinationCities = document.getElementsByClassName("destination-city-mini");
+let destinationCity = document.getElementById("destination-city")
+let destinationImage = document.getElementById("destination-image")
+let destinationSummary = document.getElementById("destination-summary")
+let tripIdeasLink = document.getElementById("trip-ideas")
+
+for(let city of destinationCities) {
+    city.addEventListener("click", (e) => {
+        let destinationId = e.target.getAttribute("destination_id");
+        let destinationSlug = e.target.getAttribute("destination_slug");
+        let summary = e.target.getAttribute("destination_summary");
+        let destinationImagePath = e.target.getAttribute("src");
+        let city = document.getElementById(`destination-city${destinationId}`).innerText;
+        destinationSummary.innerText = summary;
+        destinationCity.innerText = city;
+        destinationImage.src = destinationImagePath
+        tripIdeasLink.href = `trip_inspiration/${destinationId}/${destinationSlug}`
+
+        deleteConfirm.href = `delete_review/${reviewId}`;
+
+
+    })
+}
+
 
 let userReviews = document.getElementsByClassName("user-review");
 // let userReviewProfile = document.getElementById("solo-review-profile");

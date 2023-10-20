ef59c5e7b9b35cc013b1fe10ad6245f7c7681faa
Rob Killick <robkillick@me.com>
2023-10-20 13:27:19 +0000

    refactor: modify get places view to accept local json data rather than making API call each time

--- places/views.py
+++ places/views.py
@@ -1,4 +1,5 @@
 import numpy as np
+import json
 
 from django.shortcuts import render, get_object_or_404, reverse
 from django.http import HttpResponseRedirect
@@ -64,36 +65,17 @@ def get_places(request):
         # TRY REINSTATING THIS TRY EXCEPT STATEMENT
         # try:
 
-        # Initialize the ApifyClient with my API token
-        client = ApifyClient(os.environ.get('MY-APIFY-TOKEN'))
+        # read file
+        with open('data/los-angeles.json', 'r') as myfile:
+            data = myfile.read()
 
-        # Prepare the Actor input
-        run_input = {
-            "locationFullName": requested_location.city,
-            "maxItems": 40,
-            "language": "en",
-            "currency": "GBP",
-            "includeAttractions": True,
-            "includeHotels": False,
-            "includePriceOffers": False,
-            "includeRestaurants": True,
-            "includeTags": False,
-            "includeVacationRentals": False,
-        }
-
-        # Run the Actor and wait for it to finish
-        run = client.actor("maxcopell/tripadvisor").call(
-            run_input=run_input
-        )
-
-        # Fetch Actor results from the run's dataset (if there are any)
-        # Store data as 'places'
-        places = client.dataset(run["defaultDatasetId"]).iterate_items()
+        # parse file
+        python_data = json.loads(data)
 
         # Iterates over data retrieved from API call.
         # Checks if values for certain keys exist.
         # Returns to top of loop if any of these values are blank.
-        for place in places:
+        for place in python_data:
             required_fields = {
                 "category": place['category'],
                 "name": place['name'],
@@ -164,3 +146,133 @@ def get_places(request):
             request, 'places/get_places.html',
             context={'form': form}
         )
+
+
+"""
+ORIGINAL GET PLACES THAT MAKES CALL TO APIFY API. POSSIBLY REPLACE IN PRODUCTION
+"""
+
+
+# @staff_member_required
+# def get_places(request):
+#     """
+#     Makes a call to the Apify Trip Advisor Scraper API.  The location
+#     submitted by the form is passed to the API and recommended places
+#     based on this location are returned.  This response data is then
+#     iterated over to filter out any places that are missing necessary
+#     fields.
+#     A check is then run using the venue_id to see if the place already
+#     exists in the database.  If the place does not exist, it is saved
+#     as a new instance of Place in the database.
+#     """
+#     form = PlaceForm
+#     requested_location = {}
+#     if request.method == 'POST':
+#         form = PlaceForm(request.POST)
+
+#         if form.is_valid():
+#             requested_location = (form.cleaned_data['location'])
+
+#         # TRY REINSTATING THIS TRY EXCEPT STATEMENT
+#         # try:
+
+#         # Initialize the ApifyClient with my API token
+#         client = ApifyClient(os.environ.get('MY-APIFY-TOKEN'))
+
+#         # Prepare the Actor input
+#         run_input = {
+#             "locationFullName": requested_location.city,
+#             "maxItems": 40,
+#             "language": "en",
+#             "currency": "GBP",
+#             "includeAttractions": True,
+#             "includeHotels": False,
+#             "includePriceOffers": False,
+#             "includeRestaurants": True,
+#             "includeTags": False,
+#             "includeVacationRentals": False,
+#         }
+
+#         # Run the Actor and wait for it to finish
+#         run = client.actor("maxcopell/tripadvisor").call(
+#             run_input=run_input
+#         )
+
+#         # Fetch Actor results from the run's dataset (if there are any)
+#         # Store data as 'places'
+#         places = client.dataset(run["defaultDatasetId"]).iterate_items()
+
+#         # Iterates over data retrieved from API call.
+#         # Checks if values for certain keys exist.
+#         # Returns to top of loop if any of these values are blank.
+#         for place in places:
+#             required_fields = {
+#                 "category": place['category'],
+#                 "name": place['name'],
+#                 "description": place['description'],
+#                 "image": place['image'],
+#                 "rating": place['rating'],
+#                 "address": place['addressObj'],
+#                 "latitude": place['latitude'],
+#                 "longitude": place['longitude']
+#             }
+
+#             if not all(required_fields.values()):
+#                 continue
+
+#             # Creates an instance of Place for each place in the API response.
+#             # Populates Place fields with data from API response.
+#             place_data = Place(
+#                 location=requested_location,
+#                 venue_id=place['id'],
+#                 type=place['type'],
+#                 category=place['category'],
+#                 sub_categories=place['subcategories'],
+#                 name=place['name'],
+#                 location_string=place['locationString'],
+#                 description=place['description'],
+#                 image=place['image'],
+#                 ranking_position=place['rankingPosition'],
+#                 rating=place['rating'],
+#                 phone=place['phone'],
+#                 address=place['addressObj'],
+#                 latitude=place['latitude'],
+#                 longitude=place['longitude'],
+#                 website=place['website'],
+#                 ranking_string=place['rankingString'],
+#             )
+
+#             # Query database to see if the venue_id used in the API
+#             # response already exists. Return to top of loop if it does.
+#             venue = Place.objects.filter(
+#                 venue_id=place_data.venue_id
+#             )
+
+#             # Calculate number of words in description field using numpy
+#             word_count = np.char.count(place_data.description, ' ') + 1
+
+#             # Checks if venue already exists of description word count < 50.
+#             if venue.exists() or word_count < 30:
+#                 continue
+#             else:
+#                 # Save to database
+#                 place_data.save()
+
+#         # retrieved_places = Place.objects.filter(
+#         #     location=requested_location.id).order_by('ranking_position')
+
+#         # TRY REINSTATING THIS TRY EXCEPT STATEMENT
+
+#         # except ResponseError as error:
+#         #     raise error
+
+#         return HttpResponseRedirect(
+#             reverse('place_list', args=[
+#                 requested_location.id, requested_location.slug])
+#         )
+
+#     else:
+#         return render(
+#             request, 'places/get_places.html',
+#             context={'form': form}
+#         )
--- run.py
+++ run.py
@@ -1,24 +1,48 @@
-import numpy as np
-
-places = [
-    {"name": "Miami", "description": "", "longitude": None, "email": ""},
-    {"name": "New York", "description": "The greatest city in the world by far that's a fact", "longitude": 22.567686, "email": "robk@email.com"},
-    {"name": "New York", "description": "The greatest city in the world", "longitude": 22.567686, "email": ""}
-]
+# import numpy as np
+import json
 
 
-for place in places:
-    required_fields = {
-                "name": place['name'],
-                "description": place['description'],
-                "longitude": place['longitude']
-            }
-    word_count = np.char.count(required_fields['description'], ' ') + 1
-    print(word_count)
-    if not all(required_fields.values()) or word_count < 50:
-        print(f"skipped {required_fields['name']}")
-        continue
-    print(f"{required_fields['name']} passed check")
+# read file
+with open('data/los-angeles.json', 'r') as myfile:
+    data = myfile.read()
+
+# parse file
+python_data = json.loads(data)
+
+
+# python_data = json.loads(test.json)
+
+# print(python_data[0])
+for place in python_data:
+    print(place["name"])
+
+
+
+
+
+"""
+OLD TESTING FOR EMPTY FIELDS IN API RESPONSE
+"""
+
+# places = [
+#     {"name": "Miami", "description": "", "longitude": None, "email": ""},
+#     {"name": "New York", "description": "The greatest city in the world by far that's a fact", "longitude": 22.567686, "email": "robk@email.com"},
+#     {"name": "New York", "description": "The greatest city in the world", "longitude": 22.567686, "email": ""}
+# ]
+
+
+# for place in places:
+#     required_fields = {
+#                 "name": place['name'],
+#                 "description": place['description'],
+#                 "longitude": place['longitude']
+#             }
+#     word_count = np.char.count(required_fields['description'], ' ') + 1
+#     print(word_count)
+#     if not all(required_fields.values()) or word_count < 50:
+#         print(f"skipped {required_fields['name']}")
+#         continue
+#     print(f"{required_fields['name']} passed check")
 
 
 # if not any(place.values())

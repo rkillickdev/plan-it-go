810989a4b3f837377cbd5ac348116abd815fbba7
Rob Killick <robkillick@me.com>
2023-11-07 23:24:26 +0000

    feat: add success message for profile update view

--- profiles/views.py
+++ profiles/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.http import Http404
+from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView, UpdateView, DetailView
 from .models import Profile
 from .forms import ProfileForm
 
 
-class ProfileUpdateView(LoginRequiredMixin, UpdateView):
+class ProfileUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     """
     View to render the profile update form.
     """
@@ -15,6 +16,7 @@ class ProfileUpdateView(LoginRequiredMixin, UpdateView):
     form_class = ProfileForm
     model = Profile
     success_url = reverse_lazy("trip_list")
+    success_message = "Thanks, your profile details have been updated"
 
     def get_object(self, *args, **kwargs):
         obj = super(ProfileUpdateView, self).get_object(*args, **kwargs)

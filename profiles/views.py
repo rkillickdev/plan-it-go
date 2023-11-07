28e16bcd74dbec4627271e4edd6007630847cdc8
Rob Killick <robkillick@me.com>
2023-11-07 10:07:45 +0000

    feat: add defensive programming to ProfileUpdateView

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
+from django.http import Http404
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView, UpdateView, DetailView
 from .models import Profile
@@ -14,3 +15,9 @@ class ProfileUpdateView(LoginRequiredMixin, UpdateView):
     form_class = ProfileForm
     model = Profile
     success_url = reverse_lazy("trip_list")
+
+    def get_object(self, *args, **kwargs):
+        obj = super(ProfileUpdateView, self).get_object(*args, **kwargs)
+        if not obj.user == self.request.user:
+            raise Http404
+        return obj

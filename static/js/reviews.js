c43bd187c8b857bd34158ab41b15ed3869e45cd2
Rob Killick <robkillick@me.com>
2023-11-09 18:17:36 +0000

    feat: implement js for review template to dynamically change place name in heading

--- static/js/reviews.js
+++ static/js/reviews.js
@@ -1,5 +1,6 @@
 let editButtons = document.getElementsByClassName("btn-edit");
 let reviewHeading = document.getElementById("review-heading");
+let reviewPlace = document.getElementById("review-place");
 let reviewText = document.getElementsByTagName("textarea")[0];
 let reviewForm = document.getElementById("reviewForm");
 let submitButton = document.getElementById("submitButton");
@@ -10,10 +11,12 @@ let submitButton = document.getElementById("submitButton");
 for (let button of editButtons) {
     button.addEventListener("click", (e) => {
         let reviewId = e.currentTarget.getAttribute("review_id");
+        let placeName = e.currentTarget.getAttribute("place_name");
         let reviewContent = document.getElementById(`review-body${reviewId}`).innerText;
         reviewText.value = reviewContent;
         submitButton.value = "Update Review";
-        reviewHeading.innerText = "Update Your"
+        reviewHeading.innerText = "Update Your";
+        reviewPlace.innerText = placeName;
         reviewForm.setAttribute("action", `edit_review/${reviewId}`);
     });
 }

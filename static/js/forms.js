7fdfc537e6a7335f75ca260719a30f3787ff57ab
Rob Killick <robkillick@me.com>
2023-11-11 23:50:15 +0000

    fix: tweak html to pass validation

--- static/js/forms.js
+++ static/js/forms.js
@@ -1,10 +1,10 @@
 // Checks to see if purppose of form is to enter new details
 // or update existing details.  Changes value of submit button
-// based on the form_purpose attribute.
+// based on the data-form_purpose attribute.
 
 const submitButton = document.getElementById("submit-button");
 const formContainer = document.getElementById("form-container");
-formPurpose = formContainer.getAttribute("form_purpose");
+formPurpose = formContainer.getAttribute("data-form_purpose");
 console.log(formPurpose)
 
 document.addEventListener("DOMContentLoaded", () => {

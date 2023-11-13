2f9ee5640f8147e61f985740d8f0dbe8ec597604
Rob Killick <robkillick@me.com>
2023-11-13 16:06:07 +0000

    test: write test for get context method in places views

--- places/test_views.py
+++ places/test_views.py
@@ -3,7 +3,7 @@ from django.shortcuts import reverse
 
 from django.contrib.auth.models import User
 from profiles.models import Profile
-from .models import Place
+from .models import Place, Review, Image
 from locations.models import Location
 
 class TestPlacesViews(TestCase):
@@ -12,18 +12,45 @@ class TestPlacesViews(TestCase):
     """
 
     def setUp(self):
-        self.user = User.objects.create_superuser(username="test", password="test", email="test@test.com")
-        # self.place = Place.objects.create(
-            
-        # )
-        self.location = Location(
+        """
+        """
+        self.client = Client()
+        
+        self.user = User.objects.create_user(
+            username="testuser", email="test@test.com", password="testpassword"
+        )
+        self.user_alt = User.objects.create_user(
+            username="testuseralt", email="testalt@test.com", password="alttestpassword"
+        )
+        self.profile = Profile.objects.get(user=self.user)
+        
+        self.location = Location.objects.create(
             city="London",
             slug="london",
             summary="The Big Smoke",
             latitude=51.509865,
             longitude=-0.118092,
         )
-        self.location.save()
+
+        self.place = Place.objects.create(
+            location=self.location,
+            venue_id="1",
+            category="restaurant",
+            name="London Eye",
+            slug="london-eye",
+            description="Test Info",
+            rating=4.5,
+            address="place address",
+            latitude=51.503399,
+            longitude=-0.119519,
+        )
+
+        self.review = Review.objects.create(
+            place=self.place, profile=self.profile, body="test review"
+        )
+        self.image = Image.objects.create(
+            profile=self.profile, place=self.place
+        )
 
     
     def test_list_places(self):
@@ -40,3 +67,16 @@ class TestPlacesViews(TestCase):
         )
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'places/place_list.html')
+
+    def test_place_browse_detail(self):
+        """
+        """
+        response = self.client.get(
+            reverse('place_browse_detail',
+            kwargs={
+                "slug": self.place.slug, 
+                "pk": self.place.id,    
+            })
+        )
+        self.assertIn('reviews', response.context)
+        self.assertIn('images', response.context)

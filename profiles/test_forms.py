e08626514353ca51ad58f1a11f0d80ac45467698
Rob Killick <robkillick@me.com>
2023-11-15 17:10:04 +0000

    test: finish tests for profile forms to get 100% coverage

--- profiles/test_forms.py
+++ profiles/test_forms.py
@@ -1,9 +1,84 @@
 from django.test import TestCase
-from .forms import ProfileForm
+from .forms import ProfileForm, CustomLoginForm, CustomSignupForm
+
+
+class TestLoginForm(TestCase):
+    """
+    Tests for Custom Login Form
+    """
+    def test_login_form_invalid_required_fields_blank(self):
+        """
+        Test if user login form is invalid if any fields are blank
+        """
+        form = CustomLoginForm(
+            {
+                "login": "",
+                "password": ""
+            }
+        )
+        self.assertFalse(form.is_valid())
+        self.assertIn('login', form.errors.keys())
+        self.assertIn('password', form.errors.keys())
+        self.assertEqual(form.errors['login'][0], 'This field is required.')
+        self.assertEqual(form.errors['password'][0], 'This field is required.')
+
+class TestSignUpForm(TestCase):
+    """
+    Tests for Custom Signup Form
+    """
+    def test_sign_up_form_valid(self):
+        """
+        Tests if sign up form is valid
+        """
+        form = CustomSignupForm(
+            {
+                "username": "John",
+                "password1": "testpassword1976!",
+                "password2": "testpassword1976!"
+            }
+        )
+        self.assertTrue(form.is_valid())
+
+    def test_sign_up_form_invalid_if_required_fields_blank(self):
+        """
+        Tests if sign up form is invalid if required fields are blank
+        """
+        form = CustomSignupForm(
+            {
+                "username": "",
+                "password1": "",
+                "password2": ""
+            }
+        )
+        self.assertFalse(form.is_valid())
+        self.assertIn('username', form.errors.keys())
+        self.assertIn('password1', form.errors.keys())
+        self.assertIn('password2', form.errors.keys())
+        self.assertEqual(form.errors['username'][0], 'This field is required.')
+        self.assertEqual(form.errors['password1'][0], 'This field is required.')
+        self.assertEqual(form.errors['password2'][0], 'This field is required.')
+
+    def test_sign_up_form_invalid_if_passwords_non_matching(self):
+        """
+        Tests if sign up form is invalid if passwords do not match
+        """
+        form = CustomSignupForm(
+            {
+                "username": "John",
+                "password1": "testpassword1976!",
+                "password2": "testpassword1977!"
+            }
+        )
+        self.assertFalse(form.is_valid())
+        self.assertIn('password2', form.errors.keys())
+        self.assertEqual(form.errors['password2'][0], 'You must type the same password each time.')
 
 
 class TesTProfileForm(TestCase):
-    def test_profile_form_valid(self):
+    """
+    Tests for Profile Form
+    """
+    def test_profile_form_invalid(self):
         """
         Tests if profile form is valid
         """
@@ -17,7 +92,7 @@ class TesTProfileForm(TestCase):
         )
         self.assertTrue(form.is_valid())
 
-    def test_profile_form_invalid_max_length_exceeded(self):
+    def test_profile_form_invalid_if_max_length_exceeded(self):
         """
         Test if profile form is invalid when fields with max
         length are exceeded.
@@ -33,8 +108,20 @@ class TesTProfileForm(TestCase):
         self.assertIn('first_name', form.errors.keys())
         self.assertIn('surname', form.errors.keys())
         self.assertIn('screen_name', form.errors.keys())
+        self.assertEqual(
+            form.errors["first_name"][0],
+            "Ensure this value has at most 50 characters (it has 51).",
+        )
+        self.assertEqual(
+            form.errors["surname"][0],
+            "Ensure this value has at most 50 characters (it has 51).",
+        )
+        self.assertEqual(
+            form.errors["screen_name"][0],
+            "Ensure this value has at most 25 characters (it has 26).",
+        )
 
-    def test_profile_form_invalid_required_fields_blank(self):
+    def test_profile_form_invalid_if_required_fields_blank(self):
         """
         Test if profile form is invalid if required fields are blank
         """

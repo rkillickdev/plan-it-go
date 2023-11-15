4f9f42f8862999025c2688cc63596d77ea5e7f35
Rob Killick <robkillick@me.com>
2023-11-15 19:26:01 +0000

    feat: add 500 error page and style a;; error templates

--- home/urls.py
+++ home/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
-from .views import home_view
+from .views import home_view, error_404, error_403, error_500
 
-urlpatterns = [path("", home_view, name="home")]
+urlpatterns = [
+    path("", home_view, name="home"),
+    path("404_test", error_404, name="error_404"),
+    path("403_test", error_403, name="error_403"),
+    path("500_test", error_500, name="error_500")
+]
--- home/views.py
+++ home/views.py
@@ -6,3 +6,11 @@ def home_view(request):
     destinations = Location.objects.all()
     return render(request, "home/index.html", {"destinations": destinations})
 
+def error_404(request):
+    return render(request, "404.html")
+
+def error_403(request):
+    return render(request, "403.html")
+
+def error_500(request):
+    return render(request, "500.html")

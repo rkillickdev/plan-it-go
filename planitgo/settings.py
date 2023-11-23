932c1c3708fa586cd2d36ed33ef7c52fc670db61
Rob Killick <robkillick@me.com>
2023-11-23 22:14:38 +0000

    chore: switch debug to false in settings file

--- planitgo/settings.py
+++ planitgo/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = development
+DEBUG = False
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
--- planitgo/settings_test.py
+++ planitgo/settings_test.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = development
+DEBUG = False
 
 if development:
     ALLOWED_HOSTS = [
